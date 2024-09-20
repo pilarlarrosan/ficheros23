0726a7bd78edb77fd904c6d191e910b9e89cb86c
pilarlarrosan <pilarlarrosan@gmail.com>
2024-09-20 12:36:50 +0200

    nuevo paquete menu

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,14 @@ Menú para ejecutar los ejercicios de manejo de sistemas de ficheros
 de la UD1 (Acceso a ficheros) del módulo Acceso a Datos.
  */
 
+import menu.MenuPrincipal;
+
 public class Main {
     public static void main(String[] args) {
         boolean salir = false;
         String opcion = "";
         do {
-            System.out.println("0. Salir");
-            System.out.println("1. Ejercicio1 ficheros");
-            System.out.println("2. Ejercicio1 ficheros NIO");
-            System.out.println("3. Ejercicio2 ficheros");
-            System.out.println("4. Ejercicio2 ficheros NIO");
+            new MenuPrincipal().mostrarMenuPrincipal();
 
             opcion = libs.Leer.pedirCadena("Introduce una opción");
             switch (opcion) {
@@ -23,7 +21,6 @@ public class Main {
                     code.ej1.checkFiles();
                 }
                 case "2" -> {
-                    libs.Leer.clearScreen();
                     code.ej1NIO.checkFilesNIO();
                 }
                 case "3" -> {
@@ -33,7 +30,6 @@ public class Main {
 
                 }
                 default -> {
-                    libs.Leer.clearScreen();
                     System.out.println("Opción incorrecta");
                 }
             }
---
+++ src/menu/MenuPrincipal.java
@@ -0,0 +1,13 @@
+package menu;
+
+public class MenuPrincipal {
+    public MenuPrincipal() {}
+
+    public void mostrarMenuPrincipal() {
+        System.out.println("0. Salir");
+        System.out.println("1. Ejercicio1 ficheros");
+        System.out.println("2. Ejercicio1 ficheros NIO");
+        System.out.println("3. Ejercicio2 ficheros");
+        System.out.println("4. Ejercicio2 ficheros NIO");
+    }
+}
